58f1f16d926a29e90ed68218ee2b0760340ab0f6
ryuzera <ryugfx@gmail.com>
2025-06-11 10:31:28 -0300

    CourseService

---
+++ src/main/java/com/rocketseat/register_course/business/CourseService.java
@@ -0,0 +1,50 @@
+package com.rocketseat.register_course.business;
+
+import com.rocketseat.register_course.infrastructure.entity.Course;
+import com.rocketseat.register_course.infrastructure.exception.CourseCategoryNotFoundException;
+import com.rocketseat.register_course.infrastructure.exception.CourseNotFoundException;
+import com.rocketseat.register_course.infrastructure.repository.CourseRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class CourseService {
+
+    private final CourseRepository courseRepository;
+
+    public CourseService(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    public void saveCourse(Course course) {
+        courseRepository.saveAndFlush(course);
+    }
+
+    public void deleteCourse(Course course) {
+        courseRepository.delete(course);
+    }
+
+    public void findCourseById(Course course) {
+        courseRepository.findById(course.getId());
+    }
+
+    public Course findCoursesByCategory(String category) {
+        return courseRepository.findByCategory(category).orElseThrow(
+                () -> new CourseCategoryNotFoundException("Course category " + category + " not found")
+        );
+    }
+
+    public void updateCourseById(UUID id, Course course) {
+        Course courseToUpdate = courseRepository.findById(id).orElseThrow(() -> new CourseNotFoundException("Course not found"));
+
+        Course courseUpdated = Course.builder()
+                .id(courseToUpdate.getId())
+                .name(course.getName() != null ? course.getName() : courseToUpdate.getName())
+                .category(course.getCategory() != null ? course.getCategory() : courseToUpdate.getCategory())
+                .build();
+
+        courseRepository.saveAndFlush(courseUpdated);
+    }
+}
