872aa381b1fbaa0ebe17a6011e5740e6ca89d403
ryuzera <ryugfx@gmail.com>
2025-06-11 09:47:04 -0300

    spring initializr

---
+++ src/main/java/com/rocketseat/register_course/RegisterCourseApplication.java
@@ -0,0 +1,13 @@
+package com.rocketseat.register_course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rocketseat/register_course/RegisterCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocketseat.register_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
