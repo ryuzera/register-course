18b61d71fa5e126b3b50a1311b29b6414aa35fd7
ryuzera <ryugfx@gmail.com>
2025-06-12 15:18:06 -0300

    add CreationTimestamp in createdAt

--- src/main/java/com/rocketseat/register_course/infrastructure/entity/Course.java
+++ src/main/java/com/rocketseat/register_course/infrastructure/entity/Course.java
@@ -2,6 +2,7 @@ package com.rocketseat.register_course.infrastructure.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -25,9 +26,10 @@ public class Course {
     @Column(name = "category", nullable = false, length = 265)
     private String category;
 
-    @Column(name = "created_at", nullable = false, length = 265)
+    @CreationTimestamp
+    @Column(name = "created_at", length = 265)
     private LocalDateTime createdAt;
 
-    @Column(name = "update_at", nullable = false, length = 265)
+    @Column(name = "update_at", length = 265)
     private LocalDateTime updateAt;
 }
